5ba01b90d23b18404ed05b4fd9447cf0a6322553
KotaSakurai <sakurai.kota@mwed.co.jp>
2019-04-04 02:26:51 +0900

    make disable

--- src/content.js
+++ src/content.js
@@ -1,1 +1,9 @@
-window.alert('アプリ開いたね！');
+window.onload = function() {
+  document.getElementsByName('comment[body]').forEach(commentElement => {
+    commentElement.addEventListener('keyup', function() {
+      Array.prototype.forEach.call(document.getElementsByClassName('start-review-label'), function(element) {
+        element.parentElement.disabled = true;
+      });
+    });
+  });
+}
